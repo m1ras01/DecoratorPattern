227daf51ebc0a6fc9e01dcf8ed4432ef7f78ba87
m1ras01 <221930@astanait.edu.kz>
2023-10-03 13:01:58 +0600

    Example2

--- src/Example2/Process.java
+++ src/Example2/Process.java
@@ -2,6 +2,13 @@ package Example2;
 
 public class Process {
     public static void main(String[] args) {
+        Devoleper Junior = new JavaDev();
+        System.out.println(Junior.makeJob());
 
+        Devoleper Senior = new SeniorDev(new JavaDev());
+        System.out.println(Senior.makeJob());
+
+        Devoleper TeamLeader = new TeamLead(new SeniorDev(new JavaDev()));
+        System.out.println(TeamLeader.makeJob());
     }
 }
